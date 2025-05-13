77fd481ae1ccaee30cef0cbe6e7f854aa8a072c1
Axing <axing.cc@gmail.com>
2025-05-13 15:57:02 +0800

    ghproxy.ps1

--- ghproxy.ps1
+++ ghproxy.ps1
@@ -1,3 +1,11 @@
+function Test-CommandAvailable {
+    param (
+        [Parameter(Mandatory = $True, Position = 0)]
+        [String] $Command
+    )
+    return [Boolean](Get-Command $Command -ErrorAction SilentlyContinue)
+}
+
 # Get the config form file
 $ghfile = Convert-Path "$PSScriptRoot\ghproxy.json"
 $ghproxy = Get-Content $ghfile | ConvertFrom-Json
@@ -22,25 +30,30 @@ Write-Host "[*] fast ghproxy: $($ghproxy.result)"
 $ghproxy.update_time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 $ghproxy | ConvertTo-Json | Set-Content -Path $ghfile 
 
-# Auto scoop config
-Write-Host "[*] scoop config" 
-$scoopConfig = $(scoop config aria2-options)
-$aria2Options = "--ghproxy='$($ghproxy.result)'"
-if( $scoopConfig ) {
-    if( $scoopConfig -match '--ghproxy=' ) {
-        $scoopConfig = $scoopConfig -replace '--ghproxy=([^\s]+)',$aria2Options
-    } else {
-        $scoopConfig += " $aria2Options" 
-    }
-} else {
-    $scoopConfig = $aria2Options 
-}
-scoop config aria2-options $scoopConfig
 
+# Auto scoop config
+if (Test-CommandAvailable('scoop')) {
+    Write-Host "[*] scoop config" 
+    $scoopConfig = $(scoop config aria2-options)
+    $aria2Options = "--ghproxy='$($ghproxy.result)'"
+    if( $scoopConfig ) {
+        if( $scoopConfig -match '--ghproxy=' ) {
+            $scoopConfig = $scoopConfig -replace '--ghproxy=([^\s]+)',$aria2Options
+        } else {
+            $scoopConfig += " $aria2Options" 
+        }
+    } else {
+        $scoopConfig = $aria2Options 
+    }
+    scoop config aria2-options $scoopConfig
+}
 
 # Auto git config
-git config list | findstr ".insteadof=https://github.com/" | Foreach-Object { git config --global --unset $_.Replace('.insteadof=https://github.com/','.insteadof') }
-git config --global url."https://github.com/".insteadOf github://
-git config --global url."$($ghproxy.result)https://github.com/".insteadOf https://github.com/
-Write-Host "[*] git config"
-git config list | findstr "github" | Write-Host
+if (Test-CommandAvailable('git')) {
+    git config list | findstr ".insteadof=https://github.com/" | Foreach-Object { git config --global --unset $_.Replace('.insteadof=https://github.com/','.insteadof') }
+    git config --global url."https://github.com/".insteadOf github://
+    git config --global url."$($ghproxy.result)https://github.com/".insteadOf https://github.com/
+    Write-Host "[*] git config"
+    git config list | findstr "github" | Write-Host
+} 
+
